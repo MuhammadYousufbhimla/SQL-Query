988fb44ccd9849a0c3048e7f5b23c98356b9f00e
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2023-03-11 19:41:13 +0500

    Query works

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+use Student
+
+select *from Student_info 
+select *from teacher
+Select * from employe 
+
+Insert into teacher values (4,'Adnan','Bio-medical','032150452','42101-9813328-7');
+Insert into employe values ('Rohail	','Sale');
+s
