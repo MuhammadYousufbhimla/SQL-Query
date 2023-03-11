7673683c4c419f19c142274daee90d2583daf640
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2023-03-11 20:08:34 +0500

    Add Query

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,4 +6,46 @@ Select * from employe
 
 Insert into teacher values (4,'Adnan','Bio-medical','032150452','42101-9813328-7');
 Insert into employe values ('Rohail	','Sale');
-s
+
+delete from parents where id=2
+
+
+/* New table from script*/
+
+Create table store 
+(
+id bigint IDENTITY(1,1) PRIMARY KEY,
+ name varchar (20),
+ item varchar (10),
+
+);
+select *from store
+alter table store add quantity varchar(50)
+
+alter table store add price varchar(50)
+alter table store alter column quantity varchar(20)
+alter table store drop column quantiy
+alter table store drop column price
+insert into store values('Ismail ','Mirinda','2')
+update store set quantity='1' where id =1
+Select s.price from store s
+delete from store where id=2
+
+
+/* New customer table from script*/
+
+Create table Customer 
+(
+Cus_id bigint IDENTITY(1,1) PRIMARY KEY,
+ Cus_name varchar (20),
+ Cus_role varchar (10),
+  Cus_address varchar (10),
+  Cus_phone varchar(20)
+
+);
+
+insert into Customer values('Ahmed','Testre','Al -Asif','0321-8563142')
+insert into Customer values('Mesum','Backend','Defence','0321-8563142')
+insert into Customer values('Noman','Programmer','Johar ','0321-8563142')
+
+Select *from Customer
