8ed475370bd3232d353de2bd7fbb594e1a41dd2a
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2023-03-11 22:41:58 +0500

    add qry

--- Procedure & Functions.sql
+++ Procedure & Functions.sql
@@ -1,5 +1,7 @@
 use Student
 
+--Create one parameter procedure
+
 Create Procedure One_proc 
 @name varchar(50)
 as
@@ -9,3 +11,19 @@ Select *from customer where Cus_name  =@name
 go
 
 exec One_proc @name ='ayan'
+
+--Create Two parameter procedure
+
+Create Procedure Two_proc 
+@name varchar(50),
+@address varchar(50)
+as
+
+Select *from customer where Cus_name  =@name
+And Cus_address=@address
+go
+
+
+exec Two_proc @name ='ayan' ,@address='Malir'
+
+
