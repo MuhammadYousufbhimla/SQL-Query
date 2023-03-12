f31932668d275b44be4dbc53a97e0988a5c20918
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2023-03-12 14:17:17 +0500

    add qry

--- Procedure & Functions.sql
+++ Procedure & Functions.sql
@@ -27,3 +27,23 @@ go
 exec Two_proc @name ='ayan' ,@address='Malir'
 
 
+-- Create Functions Scalar valued
+
+Create function Addtwonumber(@a int ,@b int)
+
+Returns int as
+Begin 
+Return (@a +@b)
+end
+
+Create function addno (@p1 int,@p2 int)
+returns int
+as
+Begin
+    Return @p1 + @p2
+End
+
+	
+Select dbo.addno(23,34)
+
+select dbo.Addtwonumber(5,5)
