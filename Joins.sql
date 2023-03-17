af9098f125a7e6fc2a9fa15c375e042cc34f80b5
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2023-03-17 20:08:36 +0500

    Join working

--- Joins.sql
+++ Joins.sql
@@ -8,3 +8,49 @@ order by p.Pro_id
 
 Select *from Producct
 select *from Customer
+
+--Left join only left table field show whether right table same id else null
+
+Select p.f_name,p.l_name,p.status,pr.Cus_name from parents p
+left join Customer pr
+on p.id=pr.Cus_id
+order by p.id
+
+
+select *from parents
+select *from Customer
+
+
+--Right join only Right table field show whether left table same id else null
+
+Select p.f_name,p.l_name,p.status from parents p
+right join Customer pr
+on p.id=pr.Cus_id
+order by p.id
+
+
+select *from parents
+select *from Customer
+
+
+--Full join p.id==pr.cus_id same  else null
+
+Select p.f_name,p.l_name,p.status from parents p
+full join Customer pr
+on p.id=pr.Cus_id
+order by p.id
+
+
+select *from parents
+select *from Customer
+
+--Cross join product each row
+
+Select p.f_name,pr.Cus_name from parents p
+Cross join Customer pr
+
+order by p.id
+
+
+select *from parents
+select *from Customer
